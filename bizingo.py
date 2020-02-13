7069609f8c56e656ba48cc34c4d6c669ae0fe340
Jardel <jardelfilho2001@gmail.com>
2020-02-13 14:40:12 -0300

    Problema na variável wGreen

--- bizingo.py
+++ bizingo.py
@@ -17,7 +17,7 @@ YELLOW = (255, 255,   0)
 size = [1280, 960]
 screen = pygame.display.set_mode(size)
  
-pygame.display.set_caption("Example code for the draw module")
+pygame.display.set_caption("Bizingo")
  
 #Loop until the user clicks the close button.
 done = False
@@ -28,27 +28,35 @@ class Triangle():
     def __init__(self):
         pass
         
-    def drawUpsideTriangle(self, side, x, y):
+    def drawUpsideTriangle(self, side, x, y, hasPiece):
+        self.hasPiece = hasPiece
         self.side = side
         self.x = x
         self.y = y
         self.height = side * sqrt(3)/2
         self.endPoint = x + side
+        self.upPointUpside = [(side/2) + x, y]
+        self.leftPointUpside = [x, (self.height) + y]
+        self.rightPointUpside = [(self.endPoint), (self.height) + y]
         # Primeiro ponto: acima
         # Segundo ponto: esquerda
         # Terceiro ponto: direita
-        pygame.draw.polygon(screen, GREEN, [[(side/2) + x, y], [x, (self.height) + y], [(self.endPoint), (self.height) + y]])
+        pygame.draw.polygon(screen, GREEN, [self.leftPointUpside, self.rightPointUpside, self.upPointUpside])
 
-    def drawDownsideTriangle(self, side, x, y):
+    def drawDownsideTriangle(self, side, x, y, hasPiece):
+        self.hasPiece = hasPiece
         self.side = side
         self.x = x
         self.y = y
         self.height = side * sqrt(3)/2
         self.endPoint = x + side
+        self.leftPointDownside = [x, y]
+        self.rightPointDownside = [(self.endPoint), y]
+        self.downPointDownside = [(side/2) + x, (self.height) + y]
         # Primeiro ponto: esquerda
         # Segundo ponto: direita
         # Terceiro ponto: abaixo
-        pygame.draw.polygon(screen, BLACK, [[x, y], [(self.endPoint), y], [(side/2) + x, (self.height) + y]])
+        pygame.draw.polygon(screen, BLACK, [self.leftPointDownside, self.rightPointDownside, self.downPointDownside])
 
     def drawPiece(self, piece, center):
         self.piece = piece
@@ -66,7 +74,6 @@ class Triangle():
             pass
         
 while not done:
- 
     clock.tick(10)
      
     for event in pygame.event.get():
@@ -75,51 +82,65 @@ while not done:
  
     screen.fill(GREY)
 
-    def lineGreen(x, y, quantity, side, piece):
+    w, h = 12, 12
+    wGreen = 0
+    listTrianglesGreen = [[0 for x in range(w)] for y in range(h)]
+    def lineGreen(x, y, quantity, side):
         triangle = Triangle()
         for i in range(quantity):
-            triangle.drawUpsideTriangle(side, x, y)
-            triangle.drawPiece(piece, 1.5)
+            triangle.drawUpsideTriangle(side, x, y, False)
             x = x + side
+            listTrianglesGreen[wGreen][i] = triangle.x
 
-    def lineWhite(x, y, quantity, side, piece):
+    listTrianglesWhite = []
+    def lineWhite(x, y, quantity, side):
         triangle = Triangle()
         for i in range(quantity):
-            triangle.drawDownsideTriangle(side, x, y)
-            triangle.drawPiece(piece, 2.5)
+            triangle.drawDownsideTriangle(side, x, y, False)
             x = x + side
+            listTrianglesWhite.append(triangle)
 
     def drawBoardGreen():
         x = 350
         y = 100
         for i in range(3, 12):
-            lineGreen(x, y, i, 50, "red")
+            lineGreen(x, y, i, 50)
             x = x - 25
             y = y + (50 * sqrt(3)/2)
+            wGreen = wGreen + 1
 
         x = x + 50
         for i in range(10, 8, -1):
-            lineGreen(x, y, i, 50, "red")
+            lineGreen(x, y, i, 50)
             x = x + 25
             y = y + (50 * sqrt(3)/2)
+            wGreen = wGreen + 1
 
     def drawBoardWhite():
         x = 375
         y = 100
         for i in range(2, 12):
-            lineWhite(x, y, i, 50, "blue")
+            lineWhite(x, y, i, 50)
             x = x - 25
             y = y + (50 * sqrt(3)/2)
 
         x = x + 50
         for i in range(10, 9, -1):
-            lineWhite(x, y, i, 50, "blue")
+            lineWhite(x, y, i, 50)
             x = x + 25
             y = y + (50 * sqrt(3)/2)
 
-    drawBoardGreen()
+    # def drawInitialBlackPieces():
+    #     for i in range
 
+    drawBoardGreen()
     drawBoardWhite()
+
+    # triangle.drawPiece(piece, 1.5)
+    # triangle.drawPiece(piece, 2.5)
+
     pygame.display.flip()
  
 pygame.quit()
+
+print(listTrianglesGreen[11][1])
